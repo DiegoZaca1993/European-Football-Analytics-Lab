6ab191656d307c78704d507e073815f07062db55
DiegoZaca1993 <65996912+DiegoZaca1993@users.noreply.github.com>
2026-03-16 17:11:49 -0500

    Update init_database.sql

--- sql/schema/init_database.sql
+++ sql/schema/init_database.sql
@@ -1,1 +1,15 @@
+create table teams (
+  id serial primary key,
+  name text,
+  league text,
+  country text
+);
 
+create table players (
+  id serial primary key,
+  name text,
+  team text,
+  position text,
+  goals integer,
+  assists integer
+);
